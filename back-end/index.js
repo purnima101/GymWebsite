36139790c6d7d5ffcd40800bdbfbaec77c1fff54
Purnima <purnimaagarwal2013@gmail.com>
2024-04-20 14:30:10 +0530

    backend folder

---
+++ back-end/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+
